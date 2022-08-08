8c65c8fd1ac51d3c297e2450a258c197bc511839
josepmartorell <martorelljosep>
2022-08-08 23:34:39 +0200

    team exercise game of couples

---
+++ Exercise09/src/main/java/Exercise09/Exercise09/AppMain.java
@@ -0,0 +1,27 @@
+package Exercise09.Exercise09;
+
+import java.awt.EventQueue;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppMain 
+{
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GameCouples frame = new GameCouples();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+	}
+}
